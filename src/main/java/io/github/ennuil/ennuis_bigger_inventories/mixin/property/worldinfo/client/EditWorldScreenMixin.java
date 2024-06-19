7e52e486a05ce999cd34cbda3eda64a97e238ca8
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2024-06-19 00:38:01 -0300

    Fix Edit World's button addition

--- src/main/java/io/github/ennuil/ennuis_bigger_inventories/mixin/property/worldinfo/client/EditWorldScreenMixin.java
+++ src/main/java/io/github/ennuil/ennuis_bigger_inventories/mixin/property/worldinfo/client/EditWorldScreenMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.screen.BackupPromptScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.EditWorldScreen;
 import net.minecraft.client.gui.widget.button.ButtonWidget;
+import net.minecraft.client.gui.widget.layout.LinearLayoutWidget;
 import net.minecraft.text.Text;
 import net.minecraft.world.storage.WorldSaveStorage;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
@@ -15,6 +16,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.IOException;
@@ -31,13 +33,17 @@ public abstract class EditWorldScreenMixin extends Screen {
 	}
 
 	@Shadow
-	public static boolean createBackup(WorldSaveStorage.Session storageSession) {
-		return false;
-	}
+	@Final
+	private LinearLayoutWidget layout;
 
 	@Shadow
 	@Final
 	private BooleanConsumer callback;
+
+	@Shadow
+	public static boolean createBackup(WorldSaveStorage.Session storageSession) {
+		return false;
+	}
 
 	@Inject(
 		method = "<init>",
@@ -47,23 +53,29 @@ public abstract class EditWorldScreenMixin extends Screen {
 			ordinal = 10
 		)
 	)
-	private void addConvertToTenfoursizedButton(CallbackInfo ci) {
-		try {
-			if (!((WorldSaveSummaryExtensions) storageSession.getWorldSaveSummary(storageSession.method_54545())).ebi$isTenfoursized()) {
-				this.addDrawableSelectableElement(
-					ButtonWidget.builder(Text.translatable("selectWorld.ennuis_bigger_inventories.edit.expand_all_inventories"), button -> this.client.setScreen(new BackupPromptScreen(() -> client.setScreen(this), (backup, eraseCache) -> {
+	private void addConvertToTenfoursizedButton(CallbackInfo ci) throws IOException {
+		if (!((WorldSaveSummaryExtensions) storageSession.getWorldSaveSummary(storageSession.method_54545())).ebi$isTenfoursized()) {
+			this.layout.setSpacing(3);
+			this.layout.add(
+				ButtonWidget.builder(Text.translatable("selectWorld.ennuis_bigger_inventories.edit.expand_all_inventories"), button -> this.client.setScreen(new BackupPromptScreen(() -> client.setScreen(this), (backup, eraseCache) -> {
 						if (backup) {
 							createBackup(this.storageSession);
 						}
 
 						this.client.setScreen(ConvertToTenfoursizedWorldScreen.create(this.client, this.callback, this.storageSession));
 					}, Text.translatable("ennuis_bigger_inventories.expand_all_inventories.confirm.title"), Text.translatable("ennuis_bigger_inventories.expand_all_inventories.confirm.description"), false)))
-					.positionAndSize(this.width / 2 - 100, this.height / 4 + 120 + 5, 200, 20)
+					.width(200)
 					.build()
-				);
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+			);
+		}
+	}
+
+	@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/SpacerWidget;<init>(II)V"), index = 1)
+	private int modifyHeight(int original) throws IOException {
+		if (!((WorldSaveSummaryExtensions) storageSession.getWorldSaveSummary(storageSession.method_54545())).ebi$isTenfoursized()) {
+			return 0;
+		} else {
+			return original;
 		}
 	}
 }
