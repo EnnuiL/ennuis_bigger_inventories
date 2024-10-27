24ae4498d5671227e3844a962a87327c9c0eb8a4
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2024-10-27 20:33:53 -0300

    Further simplify buildscript!!!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,9 @@ rootProject.name = "ennuis_bigger_inventories"
 
 pluginManagement {
 	repositories {
-		maven(url = uri("https://maven.quiltmc.org/repository/release"))
+		maven(uri("https://maven.quiltmc.org/repository/release"))
 		// Currently needed for Intermediary and other temporary dependencies
-		maven(url = uri("https://maven.fabricmc.net/"))
+		maven(uri("https://maven.fabricmc.net/"))
 		gradlePluginPortal()
 	}
 }
